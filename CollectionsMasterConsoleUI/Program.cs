c5ed41d723b9c637d3874130039864ceecfaa9dd
MBurnette4120 <mikeburnette@rocketmail.com>
2020-06-26 01:56:09 -0400

    finished

--- CollectionsMasterConsoleUI/Program.cs
+++ CollectionsMasterConsoleUI/Program.cs
@@ -5,6 +5,8 @@ namespace CollectionsMasterConsoleUI
 {
     class Program
     {
+    
+        
         static void Main(string[] args)
         {
             //TODO: Follow the steps provided in the comments under each region.
@@ -12,40 +14,52 @@ namespace CollectionsMasterConsoleUI
             //Utlilize the method stubs at the bottom for the methods you must create
 
             #region Arrays
-            // Create an integer Array of size 50
-            
+            // Create an integer Array of size 50 - DONE
 
-            //Create a method to populate the number array with 50 random numbers that are between 0 and 50
-            
+            int[] numbers = new int[50];
 
-            //Print the first number of the array
 
-            //Print the last number of the array            
+            //Create a method to populate the number array with 50 random numbers that are between 0 and 50 -DONE
+            Populater(numbers);
+
+
+
+            //Print the first number of the array -------------DONE
+            Console.WriteLine($"\nIndex 0: {numbers[0]} \n----------------------");
+
+            //Print the last number of the array --------------DONE
+            Console.WriteLine($"Last index: {numbers[numbers.Length - 1]} \n---------------------------");
 
             Console.WriteLine("All Numbers Original");
-            //Use this method to print out your numbers from arrays or lists
-            //NumberPrinter();
+            //Use this method to print out your numbers from arrays or lists -----------DONE
+            NumberPrinter(numbers);
             Console.WriteLine("-------------------");
 
-            //Reverse the contents of the array and then print the array out to the console.
+            //Reverse the contents of the array and then print the array out to the console.-----------DONE
             //Try for 2 different ways
             /*     Hint: Array._____(); Create a custom method     */
+            Array.Reverse(numbers);
 
             Console.WriteLine("All Numbers Reversed:");
+            NumberPrinter(numbers);
 
             Console.WriteLine("---------REVERSE CUSTOM------------");
-
+            ReverseArray(numbers);
+            NumberPrinter(numbers);
             Console.WriteLine("-------------------");
 
-            //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers
+            //Create a method that will set numbers that are a multiple of 3 to zero then print to the console all numbers------------DONE
             Console.WriteLine("Multiple of three = 0: ");
-            
-
+            ThreeKiller(numbers);
+            NumberPrinter(numbers);
+            Console.WriteLine();      
             Console.WriteLine("-------------------");
 
             //Sort the array in order now
             /*      Hint: Array.____()      */
             Console.WriteLine("Sorted numbers:");
+            Array.Sort(numbers);
+            NumberPrinter(numbers);
             
 
             Console.WriteLine("\n************End Arrays*************** \n");
@@ -56,45 +70,54 @@ namespace CollectionsMasterConsoleUI
 
             /*   Set Up   */
             //Create an integer List
-            
+            List<int> numberList = new List<int>();
+
 
             //Print the capacity of the list to the console
-            
+            Console.WriteLine($"List starting capacity: {numberList.Capacity}");
+
 
             //Populate the List with 50 random numbers between 0 and 50 you will need a method for this            
-            
+            Populater(numberList);
 
             //Print the new capacity
-            
+            Console.WriteLine($"List capacity after population: {numberList.Capacity}");
 
             Console.WriteLine("---------------------");
 
             //Create a method that prints if a user number is present in the list
             //Remember: What if the user types "abc" accident your app should handle that!
             Console.WriteLine("What number will you search for in the number list?");
-            
+            int searchNumber = int.Parse(Console.ReadLine());
+            NumberChecker(numberList, searchNumber);            
             Console.WriteLine("-------------------");
 
             Console.WriteLine("All Numbers:");
             //Print all numbers in the list
+            NumberPrinter(numberList);
             //NumberPrinter();
             Console.WriteLine("-------------------");
 
             //Create a method that will remove all odd numbers from the list then print results
             Console.WriteLine("Odds Only!!");
-            
+            OddKiller(numberList);
+            NumberPrinter(numberList);
             Console.WriteLine("------------------");
 
             //Sort the list then print results
             Console.WriteLine("Sorted Odds!!");
-            
+            numberList.Sort();
+            NumberPrinter(numberList);
             Console.WriteLine("------------------");
 
+
             //Convert the list to an array and store that into a variable
-            
+            var arrayFromList = numberList.ToArray();
+
 
             //Clear the list
-            
+            numberList.Clear();
+
 
             #endregion
         }
